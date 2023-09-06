2a30e7de1c81aa8490d6360fbab0d7e276445cb6
Reginald Mejio <rgnldmjio@gmail.com>
2023-09-06 21:28:23 +0800

    Make some change on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
   return (
     <>
       <div>
+        <div>Hello World</div>
+        <h1>Kaya ko to</h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
